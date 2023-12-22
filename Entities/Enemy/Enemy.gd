1e0ecad4bb45a9501d7bb7cf9436d7ef19965b2d
Cleyson Braga de Oliveira <Cleysonb.Oliveira@gmail.com>
2023-12-22 20:18:52 -0300

    grid change

--- Entities/Enemy/Enemy.gd
+++ Entities/Enemy/Enemy.gd
@@ -2,7 +2,7 @@ extends PathFollow3D
 
 ## Speed modifier,
 ## represents how many m/s the enemy will be doing
-@export_range(0.1, 300.0) var speed_modifier: float = 20
+@export_range(0.1, 300.0) var speed_modifier: float = 2
 
 @onready var base = get_tree().get_first_node_in_group("BASE")
 
--- Entities/RayPickerCamera/ray_picker_camera.gd
+++ Entities/RayPickerCamera/ray_picker_camera.gd
@@ -1,14 +1,24 @@
 extends Camera3D
 
+@export var gridmap: GridMap
 @export var ray_cast_Z_distance: float = 100.0
 @onready var ray_cast_3d = $RayCast3D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	var target = project_local_ray_normal(mouse_position) * ray_cast_Z_distance
 	ray_cast_3d.target_position = target
 	ray_cast_3d.force_raycast_update()
-	printt(ray_cast_3d.get_collider(), ray_cast_3d.get_collision_point())
-	#print(target)
+	
+	#printt(ray_cast_3d.get_collider(), ray_cast_3d.get_collision_point())
+	
+	if ray_cast_3d.is_colliding():
+		var collider = ray_cast_3d.get_collider()
+		if collider is GridMap:
+			#var _collision_point = ray_cast_3d.get_collision_point()
+			var _cell = gridmap.local_to_map(ray_cast_3d.get_collision_point())
+			if gridmap.get_cell_item(_cell) == 0:
+				gridmap.set_cell_item(_cell, 1)
+			
